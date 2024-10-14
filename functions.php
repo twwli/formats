47e314f8d11949b08e7105bb5a820e308633f822
twwli <hello@theworldwelivein.co.uk>
2024-10-14 15:00:09 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -277,15 +277,15 @@ function disable_emojis_tinymce( $plugins ) {
 function remove_menus(){
     //remove_menu_page( 'index.php' );                  //Dashboard
     //remove_menu_page( 'jetpack' );                    //Jetpack* 
-    //remove_menu_page( 'edit.php' );                   //Posts
+    remove_menu_page( 'edit.php' );                   //Posts
     //remove_menu_page( 'upload.php' );                 //Media
     //remove_menu_page( 'edit.php?post_type=page' );    //Pages
-    //remove_menu_page( 'edit-comments.php' );          //Comments
-    //remove_menu_page( 'themes.php' );                 //Appearance
-    //remove_menu_page( 'plugins.php' );                //Plugins
+    remove_menu_page( 'edit-comments.php' );          //Comments
+    remove_menu_page( 'themes.php' );                 //Appearance
+    remove_menu_page( 'plugins.php' );                //Plugins
     //remove_menu_page( 'users.php' );                  //Users
-    //remove_menu_page( 'tools.php' );                  //Tools
-    //remove_menu_page( 'options-general.php' );        //Settings
+    remove_menu_page( 'tools.php' );                  //Tools
+    remove_menu_page( 'options-general.php' );        //Settings
 }
 
 add_action( 'admin_menu', 'remove_menus' );
@@ -293,8 +293,8 @@ add_action( 'admin_menu', 'remove_menus' );
 
 /************* REMOVE DEFAULT WP WIDGETS *************/
 
-//remove_action( 'init', 'wp_widgets_init', 1 );
-//add_action( 'init', function() { do_action( 'widgets_init' ); }, 1 );
+remove_action( 'init', 'wp_widgets_init', 1 );
+add_action( 'init', function() { do_action( 'widgets_init' ); }, 1 );
 
 /************* Exclude pages from WordPress Search *****************/
 
