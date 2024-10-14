6c576ec82b30613efd51fa6b299f5659a433f628
twwli <hello@theworldwelivein.co.uk>
2024-10-14 14:57:38 +0200

    Create Homepage

--- template-homepage.php
+++ template-homepage.php
@@ -7,7 +7,7 @@
 <?php get_header(); ?>
 
 <main class="homepage page-content">
-<?php get_template_part('/includes/home-intro'); ?>
+    <?php get_template_part('/includes/home-intro'); ?>
 </main>
 
 <?php get_footer(); ?>
