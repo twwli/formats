862c71188dfb1858d1d5e9e836f82e3c26d27953
twwli <hello@theworldwelivein.co.uk>
2024-10-18 07:29:28 +0200

    Update template-about.php

--- template-about.php
+++ template-about.php
@@ -6,9 +6,143 @@
 
 <?php get_header(); ?>
 
-<main class="about-page page-content">
-	<?php include (TEMPLATEPATH . '/includes/onepage-introduction.php'); ?>
-	<?php include (TEMPLATEPATH . '/includes/onepage-aboutus.php'); ?>
-</main>
+    <div class="macbook-pro">
+      <div class="overlap-wrapper">
+        <div class="overlap">
+          <img class="group" src="https://c.animaapp.com/RaG1aIGc/img/group-23.png" />
+          <div class="div">
+            <img class="rectangle" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-1@2x.png" />
+            <img class="img" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-3@2x.png" />
+            <img class="rectangle-2" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-7@2x.png" />
+            <img class="line" src="https://c.animaapp.com/RaG1aIGc/img/line-4.svg" />
+            <img class="line-2" src="https://c.animaapp.com/RaG1aIGc/img/line-3.svg" />
+          </div>
+          <div class="group-2">
+            <img class="rectangle-3" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-9.png" />
+            <img class="line-3" src="https://c.animaapp.com/RaG1aIGc/img/line-9.svg" />
+            <div class="group-3">
+              <div class="frame">
+                <img class="rectangle-4" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-45@2x.png" />
+                <img class="rectangle-5" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-46@2x.png" />
+                <img class="rectangle-6" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-44@2x.png" />
+              </div>
+              <img class="line-4" src="https://c.animaapp.com/RaG1aIGc/img/line-14.svg" />
+              <img class="line-5" src="https://c.animaapp.com/RaG1aIGc/img/line-15.svg" />
+            </div>
+            <div class="overlap-group">
+              <img class="line-6" src="https://c.animaapp.com/RaG1aIGc/img/line-11.svg" />
+              <div class="le-festival">LE FESTIVAL</div>
+            </div>
+            <p class="le-format-s-festival">
+              <span class="text-wrapper"
+                >Le Format(s) Festival est un événement majeur à Strasbourg, dédié à la célébration des multiples formes
+                d&#39;expression visuelle et graphique. Il rassemble créateurs, designers, artistes et passionnés autour
+                de projets qui explorent les frontières de l&#39;image, du design et de la typographie. Chaque édition
+                du festival propose une riche programmation faite d&#39;expositions, de conférences, de workshops et de
+                performances, permettant aux visiteurs d&#39;interagir avec des œuvres innovantes et de découvrir des
+                créations visuelles inédites.<br
+              /></span>
+              <span class="span"><br /></span>
+              <span class="text-wrapper"
+                >Le festival met en lumière les nouvelles tendances et les formats émergents, et constitue un lieu de
+                rencontre privilégié pour les professionnels du milieu et le grand public curieux d&#39;explorer les
+                multiples facettes du design contemporain.</span
+              >
+            </p>
+          </div>
+          <div class="group-4">
+            <img class="rectangle-7" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-8.png" />
+            <div class="overlap-2">
+              <img class="line-7" src="https://c.animaapp.com/RaG1aIGc/img/line-8.svg" />
+              <div class="le-projet">LE PROJET</div>
+            </div>
+            <p class="suite-au-format-s">
+              <span class="text-wrapper"
+                >Suite au Format(s) Festival de Strasbourg, un workshop a été organisé à LISAA Strasbourg pour explorer
+                les différentes manières de retranscrire cet événement unique. Notre équipe, spécialisée dans la
+                création de sites web interactifs, a décidé de concevoir une plateforme originale permettant de revivre
+                le festival à travers un format ludique et immersif.<br
+              /></span>
+              <span class="span"><br /></span>
+              <span class="text-wrapper"
+                >Le site que nous avons développé repose sur une expérience interactive. Sur la page d&#39;accueil, des
+                cartes sont disposées face cachée. Chaque carte, une fois retournée, dévoile une image emblématique du
+                festival, capturant des moments forts, des lieux ou des événements qui se sont déroulés. Ces images
+                plongent l&#39;utilisateur dans l&#39;atmosphère du festival, lui donnant l&#39;impression d&#39;y être
+                et l&#39;invitant à explorer librement.<br
+              /></span>
+              <span class="span"><br /></span>
+              <span class="text-wrapper"
+                >Chaque découverte devient une nouvelle porte ouverte vers un souvenir, une émotion ou une curiosité,
+                encourageant l&#39;utilisateur à retourner chaque carte pour explorer le festival sous un nouvel angle.
+                Nous vous invitons à tenter l&#39;expérience et à plonger, vous aussi, dans cet univers visuel et
+                interactif !<br
+              /></span>
+              <span class="span"><br /></span>
+              <span class="text-wrapper"
+                >Cliquez sur les cartes et laissez-vous surprendre. <br />Le festival n’attend que vous pour être
+                redécouvert !</span
+              >
+            </p>
+            <img class="line-8" src="https://c.animaapp.com/RaG1aIGc/img/line-3-1.svg" />
+          </div>
+          <div class="group-5">
+            <div class="overlap-3">
+              <img class="line-9" src="https://c.animaapp.com/RaG1aIGc/img/line-12.svg" />
+              <div class="l-quipe">L’ÉQUIPE</div>
+            </div>
+            <img class="line-10" src="https://c.animaapp.com/RaG1aIGc/img/line-14.svg" />
+            <img class="line-11" src="https://c.animaapp.com/RaG1aIGc/img/line-13.svg" />
+            <img class="line-12" src="https://c.animaapp.com/RaG1aIGc/img/line-15.svg" />
+            <p class="nous-sommes-six-l">
+              Nous sommes six élèves de troisième année en Bachelor de Design Graphique : Émilie Boldarino, Elise Klein,
+              Baptiste Fritz, Louis Roy, Thaïs Gaubert et Callum McKenna. Durant ces cinq jours de travail intensif,
+              nous avons pu allier nos compétences et notre créativité pour concevoir ce projet, sous la supervision
+              précieuse d&#39;Olivier Guillard. Directeur artistique, UX/UI designer, développeur front-end et
+              enseignant à LISAA Strasbourg depuis 2021, il nous a guidés tout au long du processus, apportant son
+              expertise et son regard professionnel pour enrichir notre démarche.
+            </p>
+            <div class="frame-2">
+              <div class="frame-3">
+                <div class="frame-4">
+                  <img class="rectangle-8" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-41@2x.png" />
+                  <div class="div-2">KLEIN<br />Elise</div>
+                </div>
+                <div class="frame-5">
+                  <img class="rectangle-9" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-42@2x.png" />
+                  <div class="div-2">BOLDARINO&nbsp;&nbsp;<br />Émilie</div>
+                </div>
+                <div class="frame-6">
+                  <img class="rectangle-10" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-40@2x.png" />
+                  <p class="div-2">
+                    <span class="text-wrapper-2">McKenna<br /></span> <span class="text-wrapper-2">Callum </span>
+                  </p>
+                </div>
+              </div>
+              <div class="frame-7">
+                <div class="frame-4">
+                  <img class="rectangle-11" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-46-1@2x.png" />
+                  <div class="div-2">GAUBERT<br />Thaïs</div>
+                </div>
+                <div class="frame-8">
+                  <img class="p" src="https://c.animaapp.com/RaG1aIGc/img/p1033862@2x.png" />
+                  <div class="div-2">ROY<br />Louis</div>
+                </div>
+                <div class="frame-8">
+                  <img class="rectangle-12" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-47@2x.png" />
+                  <div class="div-2">FRITZ<br />Baptiste</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="frame-9">
+            <img class="rectangle-13" src="https://c.animaapp.com/RaG1aIGc/img/rectangle-39@2x.png" />
+            <div class="div-2">GUILLARD<br />Olivier</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
 
 <?php get_footer(); ?>
